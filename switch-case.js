f5595ded188eee12acb084d2834e15d107971ae9
danangace <danangbahari89@gmail.com>
2019-05-23 13:19:26 +0700

    tugas hari selasa

---
+++ switch-case.js
@@ -0,0 +1,48 @@
+var hari = 2;       //assign nilai var hari dari angka 1-31
+var bulan = 12;      //assign nilai var bulan dari angka 1-12
+var tahun = 1995;   //assign nilai var tahun dari angka 1900 - 2200
+``
+switch (bulan){
+    case 1:
+        console.log(hari + ' Januari ' + tahun);
+        break;
+    case 2:
+        console.log(hari + ' Februari ' + tahun);
+        break;
+    case 2:
+        console.log(hari + ' Februari ' + tahun);
+        break;
+    case 3:
+        console.log(hari + ' Maret ' + tahun);
+        break;
+    case 4:
+        console.log(hari + ' April ' + tahun);
+        break;
+    case 5:
+        console.log(hari + ' Mei ' + tahun);
+        break;
+    case 6:
+        console.log(hari + ' Juni ' + tahun);
+        break;
+    case 7:
+        console.log(hari + ' Juli ' + tahun);
+        break;
+    case 8:
+        console.log(hari + ' Agustus ' + tahun);
+        break;
+    case 9:
+        console.log(hari + ' September ' + tahun);
+        break;
+    case 10:
+        console.log(hari + ' Oktober ' + tahun);
+        break;
+    case 11:
+        console.log(hari + ' November ' + tahun);
+        break;
+    case 12:
+        console.log(hari + ' Desember ' + tahun);
+        break;
+    default:
+        console.log('Assign nilai variable sesuai perintah!')
+        break;
+}
